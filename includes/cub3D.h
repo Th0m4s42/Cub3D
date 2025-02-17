9167e0cc344d881f23472718d1c77dda1b559226
thbasse <thbasse@student.42.fr>
2025-02-17 08:55:55 +0100

    La base

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,21 +6,33 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 08:04:24 by thbasse           #+#    #+#             */
-/*   Updated: 2025/02/17 08:15:45 by thbasse          ###   ########.fr       */
+/*   Updated: 2025/02/17 08:52:00 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include <mlx_int.h>
-# include <mlx.h>
-# include <libft.h>
+# include "../minilibx-linux/mlx_int.h"
+# include "../minilibx-linux/mlx.h"
+# include "../libft/includes/libft.h"
 # include <math.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+
+////////////////////////////////////////////////////////////////////////////////
+//								STRUCTURES									  //
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct	s_game
+{
+}				t_game;
+
+////////////////////////////////////////////////////////////////////////////////
+//								FONCTIONS									  //
+////////////////////////////////////////////////////////////////////////////////
 
 #endif
