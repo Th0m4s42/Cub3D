54446dbaae955ee40bc3e91549dfda993f76ea24
Curry-san <chenner93@gmail.com>
2025-02-19 14:46:36 +0100

    plus qu'a mapper

--- mandatory_srcs/parsing/ft_parsing.c
+++ mandatory_srcs/parsing/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: curry-san <curry-san@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 15:13:12 by ckenaip           #+#    #+#             */
-/*   Updated: 2025/02/19 13:52:14 by curry-san        ###   ########.fr       */
+/*   Updated: 2025/02/19 14:45:07 by curry-san        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,12 @@ static bool	save_visual(t_game *game, char **line, int fd)
 	while (*line)
 	{
 		y = 0;
-		while (in_charset(*(line[y]), " \t\v\f\r\n") == true)
+		while (in_charset((*line)[y], " \t\v\f\r\n") == true)
 			y++;
-		if (in_charset(*(line[y]), "01") && all_identifier_set(game->visual))
+		if (in_charset((*line)[y], "01") && all_identifier_set(game->visual))
 			break ;
 		if (check_id(*line, y) == false)
-			return (free(*line), false);//penser a free
+			return (free(*line), false);//penser à modif gnl
 		while (checker_identifier[++i])
 		{
 			if (checker_identifier[i](game, *line) == false)
@@ -55,33 +55,10 @@ static bool	save_visual(t_game *game, char **line, int fd)
 static bool	check_config_file(t_game *game, int fd)
 {
 	char		*line;
-	// int			i;
-	// size_t		y;
-	// t_array		checker_identifier[7];
 
-	// init_function_array(&checker_identifier);
-	// i = -1;
 	line = get_next_line(fd);
 	if (save_visual(game, &line, fd) == false)
 		return (false);
-	// while (line)
-	// {
-	// 	y = 0;
-	// 	while (in_charset(line[y], " \t\v\f\r\n") == true)
-	// 		y++;
-	// 	if (in_charset(line[y], "01") && all_identifier_set(game->visual))
-	// 		break ;
-	// 	if (check_id(line, y) == false)
-	// 		return (free(line), false);//penser a free
-	// 	while (checker_identifier[++i])
-	// 	{
-	// 		if (checker_identifier[i](game, line) == false)
-	// 			return (free(line), false);
-	// 	}
-	// 	i = -1;
-	// 	free(line);
-	// 	line = get_next_line(fd);
-	// }
 	printf("trying to save map\n");
 	while (line)
 	{
