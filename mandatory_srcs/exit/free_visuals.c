0dfaf84cc369c7e0920232c881aa890f6f47a39b
thbasse <thbasse@student.42.fr>
2025-02-21 12:38:49 +0100

    p'tite update

--- mandatory_srcs/exit/free_visuals.c
+++ mandatory_srcs/exit/free_visuals.c
@@ -6,24 +6,11 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 07:40:55 by thbasse           #+#    #+#             */
-/*   Updated: 2025/02/21 12:29:06 by thbasse          ###   ########.fr       */
+/*   Updated: 2025/02/21 12:38:38 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
-
-
-static void	ft_free_visuals(t_game *game)
-{
-	if (game->visual.north)
-		free(game->visual.north);
-	if (game->visual.south)
-		free(game->visual.south);
-	if (game->visual.west)
-		free(game->visual.west);
-	if (game->visual.east)
-		free(game->visual.east);
-}
 
 static void	ft_free_map(char **map)
 {
@@ -41,5 +28,4 @@ void	ft_free_game(t_game *game)
 {
 	mlx_destroy_window(game->mlx, game->win);
 	ft_free_map(game->map);
-	// ft_free_visuals(game);
 }
