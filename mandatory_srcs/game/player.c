d264aa86ec21305aed783e16f56e230672e3a13e
thbasse <thbasse@student.42.fr>
2025-02-24 09:32:18 +0100

    le joueur est au centre de la case comme le veux Guigui

--- mandatory_srcs/game/player.c
+++ mandatory_srcs/game/player.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 11:30:09 by thbasse           #+#    #+#             */
-/*   Updated: 2025/02/24 08:56:57 by thbasse          ###   ########.fr       */
+/*   Updated: 2025/02/24 09:24:36 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,8 @@ void	init_player(t_game *game)
 		game->player.dirx -= 1;
 	game->player.planex = 0.0;
 	game->player.planey = 0.66;
+	game->player.x += 0.5;
+	game->player.y += 0.5;
 	game->player.key_up = false;
 	game->player.key_down = false;
 	game->player.key_right = false;
