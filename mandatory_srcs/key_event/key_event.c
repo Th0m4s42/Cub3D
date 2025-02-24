7f6353b1d93adad64955813c51cf09a8161a98f1
Curry-san <chenner93@gmail.com>
2025-02-24 12:48:43 +0100

    blops

--- mandatory_srcs/key_event/key_event.c
+++ mandatory_srcs/key_event/key_event.c
@@ -6,7 +6,7 @@
 /*   By: curry-san <curry-san@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 11:46:33 by curry-san         #+#    #+#             */
-/*   Updated: 2025/02/21 14:39:57 by curry-san        ###   ########.fr       */
+/*   Updated: 2025/02/24 12:47:39 by curry-san        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,38 @@
 int		key_press(int keycode, t_game *game)
 {
 	if (keycode == ESC_KEY)
-	{
 		mlx_loop_end(game->mlx);
-	}
+	else if (keycode == KEY_LEFT)
+		printf("keyLEFT\n");
+	else if (keycode == KEY_RIGHT)
+		printf("keyRIGHT\n");
+	else if (keycode == KEY_W)
+		printf("key_W\n");
+	else if (keycode == KEY_A)
+		printf("key_A\n");
+	else if (keycode == KEY_S)
+		printf("key_S\n");
+	else if (keycode == KEY_D)
+		printf("key_D\n");
+	return (0);
+}
+
+int		key_press(int keycode, t_game *game)
+{
+	if (keycode == ESC_KEY)
+		mlx_loop_end(game->mlx);
+	else if (keycode == KEY_LEFT)
+		printf("keyLEFT\n");
+	else if (keycode == KEY_RIGHT)
+		printf("keyRIGHT\n");
+	else if (keycode == KEY_W)
+		printf("key_W\n");
+	else if (keycode == KEY_A)
+		printf("key_A\n");
+	else if (keycode == KEY_S)
+		printf("key_S\n");
+	else if (keycode == KEY_D)
+		printf("key_D\n");
 	return (0);
 }
 
@@ -37,7 +66,7 @@ void	init_key_event(t_game *game)
 {
 	(void)game;
 	mlx_hook(game->win, 2, 1L << 0, &key_press, game);
-	// mlx_hook(game->win, 3, 1L << 1, &key_release, game);
+	mlx_hook(game->win, 3, 1L << 1, &key_release, game);
 	mlx_hook(game->win, 17, 1L << 19, &cross_event, game);
 	// mlx_set_font(game->mlx, game->win,
 	// 	"-sony-fixed-medium-r-normal--24-170-100-100-c-120-iso8859-1");
