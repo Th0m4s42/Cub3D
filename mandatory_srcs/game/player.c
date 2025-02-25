f14cd1682e0f3044ad4502ac12a9426949839a25
thbasse <thbasse@student.42.fr>
2025-02-25 09:52:10 +0100

    Attention les yeux, ca marche ; git push

--- mandatory_srcs/game/player.c
+++ mandatory_srcs/game/player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: curry-san <curry-san@student.42.fr>        +#+  +:+       +#+        */
+/*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 11:30:09 by thbasse           #+#    #+#             */
-/*   Updated: 2025/02/24 12:55:51 by curry-san        ###   ########.fr       */
+/*   Updated: 2025/02/25 09:51:19 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ t_player	player_start(char **map)
 	int	y;
 	t_player	player;
 
+	ft_bzero(&player, sizeof(t_player));
 	y = 0;
 	while (map[y] && map[y + 1])
 	{
@@ -34,8 +35,6 @@ t_player	player_start(char **map)
 			}
 			y++;
 		}
-	player.dirx = 0.0;
-	player.diry = 0.0;
 	return (player);
 }
 
@@ -43,21 +42,25 @@ void	init_player(t_game *game)
 {
 	game->player = player_start(game->map);
 	if(game->map[(int)game->player.y][(int)game->player.x] == 'N')
+	{
 		game->player.diry -= 1;
+		game->player.planex = 0.66;
+	}
 	else if(game->map[(int)game->player.y][(int)game->player.x] == 'S')
+	{
 		game->player.diry += 1;
+		game->player.planex = -0.66;
+	}
 	else if(game->map[(int)game->player.y][(int)game->player.x] == 'E')
+	{
 		game->player.dirx += 1;
+		game->player.planey = 0.66;
+	}
 	else if(game->map[(int)game->player.y][(int)game->player.x] == 'W')
+	{
 		game->player.dirx -= 1;
-	game->player.planex = 0.0;
-	game->player.planey = 0.66;
+		game->player.planey = -0.66;
+	}
 	game->player.x += 0.5;
 	game->player.y += 0.5;
-	game->player.key_up = false;
-	game->player.key_down = false;
-	game->player.key_right = false;
-	game->player.key_left = false;
-	game->player.camera_left = false;
-	game->player.camera_right = false;
 }
