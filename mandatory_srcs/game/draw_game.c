d48ff2cefa39f0676a0a76e6b476777ef1e1c173
Thomas Basse <thomasbasse95@gmail.com>
2025-03-04 16:12:48 +0100

    ca marche du sol au plafond

--- mandatory_srcs/game/draw_game.c
+++ mandatory_srcs/game/draw_game.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw_game.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: curry-san <curry-san@student.42.fr>        +#+  +:+       +#+        */
+/*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 10:29:38 by thbasse           #+#    #+#             */
-/*   Updated: 2025/03/02 00:31:01 by curry-san        ###   ########.fr       */
+/*   Updated: 2025/03/04 16:10:40 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,35 @@ void	draw_line(t_game *game, int start, int end, int x, int color)
 	}
 }
 
+static void	draw_floor_ceiling(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while(y < HEIGHT / 2)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			put_pixel(&game->key_img, x, y, 0);
+			x++;
+		}
+		y++;
+	}
+	y = HEIGHT / 2;
+	while(y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			put_pixel(&game->key_img, x, y, 0);
+			x++;
+		}
+		y++;
+	}
+}
+
 int	draw_wall(t_game *game)
 {
 	int	line_height;
@@ -50,24 +79,25 @@ int	draw_wall(t_game *game)
 	if (game->player.key_right == true)
 		game->player.x += 0.01;
 	// printf("player_X = %f, player_Y = %f\n", game->player.x, game->player.y);
-	// while (x < WIDTH)
-	// {
-	// 	ray = init_ray(game, x);
-	// 	dda(&ray, game);
-	// 	line_height = (int)(HEIGHT / ray.perpwalldist);
-	// 	draw_start = -line_height / 2 + HEIGHT / 2;
-	// 	if (draw_start < 0)
-	// 		draw_start = 0;
-	// 	draw_end = line_height / 2 + HEIGHT / 2;
-	// 	if (draw_end >= HEIGHT)
-	// 		draw_end = HEIGHT - 1;
-	// 	if (ray.side == 1)
-	// 		color = 0xFF0000; // Red for y-side walls
-	// 	else
-	// 		color = 0x00FF00; // Green for x-side walls
-	// 	draw_line(game, draw_start, draw_end, x, color);
-	// 	x++;
-	// }
+	draw_floor_ceiling(game);
+	while (x < WIDTH)
+	{
+		ray = init_ray(game, x);
+		dda(&ray, game);
+		line_height = (int)(HEIGHT / ray.perpwalldist);
+		draw_start = -line_height / 2 + HEIGHT / 2;
+		if (draw_start < 0)
+			draw_start = 0;
+		draw_end = line_height / 2 + HEIGHT / 2;
+		if (draw_end >= HEIGHT)
+			draw_end = HEIGHT - 1;
+		if (ray.side == 1)
+			color = 0xFF0000; // Red for y-side walls
+		else if (ray.side == 0)
+			color = 0x00FF00; // Green for x-side walls
+		draw_line(game, draw_start, draw_end, x, color);
+		x++;
+	}
 	mlx_put_image_to_window(game->mlx, game->win, game->key_img.img, 0, 0);
 	draw_map(game);// APPUIE SUR M !
 	return (0);
