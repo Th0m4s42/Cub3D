ce344f68050a50f6ced185dbc942110fe5289cea
Curry-san <chenner93@gmail.com>
2025-03-25 21:04:33 +0100

    prep 2

--- mandatory_srcs/game/draw_texture.c
+++ mandatory_srcs/game/draw_texture.c
@@ -6,16 +6,34 @@
 /*   By: curry-san <curry-san@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 20:14:12 by curry-san         #+#    #+#             */
-/*   Updated: 2025/03/25 20:21:13 by curry-san        ###   ########.fr       */
+/*   Updated: 2025/03/25 21:00:52 by curry-san        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
 
+t_tex	get_texture(t_game *game, t_ray *ray)
+{
+	return game->north;
+	if (ray->side == 1)
+	{
+		if (ray->raydiry > 0)
+			return (game->south);
+		return (game->north)
+	}
+	else if (ray->side == 0)
+	{
+		if (ray->raydirx > 0)
+			return (game->east);
+		return (game->west);
+	}
+}
+
 void	draw_texture(t_game *game, t_ray *ray)
 {
+	t_tex	texture;
 	(void)game;
-	(void)ray;
-	
+	texture = get_texture(game, ray);
+	(void)texture;
 	return ;
 }
